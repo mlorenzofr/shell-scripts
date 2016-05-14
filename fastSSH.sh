672d78d4ccc9db3d51e6da5aa9c1f6a65fe1db26
Manuel Lorenzo Frieiro <mlorenzofr@gmail.com>
2016-05-15 01:51:08 +0200

    Tiny wrapper for my ssh connections

---
+++ fastSSH.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+try=0
+
+while [ ${try} -lt 4 ]; do
+	if [ ${try} -eq 2 ]; then
+		options="-o PubkeyAuthentication=no"
+    elif [ ${try} -eq 3 ]; then
+		options="-o PubkeyAuthentication=no -l root"
+	fi
+	ssh -X ${options} $(basename ${0}) $@ && break
+	try=$(${try}+1)
+done
